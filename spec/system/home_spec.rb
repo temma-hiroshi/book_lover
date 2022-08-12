3e0d835dda5b7b3b956dfc4510e6514ac1c0715e
天摩洋 <chinmi@tenmayounoMacBook-Pro.local>
2022-08-12 11:22:26 +0900

    RSpec,systemspecを使うための記述を追記

---
+++ spec/system/home_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+RSpec.describe "Home", type: :system do
+  before do
+    visit home_index_path
+  end
+
+  describe "GET /home" do
+    it "あなたの好きを、誰かの好きに。と表示すること" do
+      expect(page).to have_content("あなたの好きを、誰かの好きに")
+    end
+  end
+
+end
