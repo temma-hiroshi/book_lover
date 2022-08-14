2b16acac5830c608336480bb0fbd51af89761d17
天摩洋 <chinmi@tenmayounoMacBook-Pro.local>
2022-08-14 11:06:14 +0900

    heroku pushエラー解消のため、gitignoreを修正

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -2,10 +2,11 @@ const { environment } = require('@rails/webpacker')
 
 const webpack = require('webpack')
 environment.plugins.prepend('Provide',
-  new webpack.ProvidePlugin({
-    $: 'jquery/src/jquery',
-    jQuery: 'jquery/src/jquery'
-  })
+    new webpack.ProvidePlugin({
+        $: 'jquery/src/jquery',
+        jQuery: 'jquery/src/jquery',
+        Popper: 'popper.js'
+    })
 )
 
 module.exports = environment
