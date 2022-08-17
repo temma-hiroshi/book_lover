eba2cd247c198224e2cc2f9a50f63093dfe8db37
天摩洋 <chinmi@tenmayounoMacBook-Pro.local>
2022-08-17 14:31:39 +0900

    users#destroyの処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,12 @@ class UsersController < ApplicationController
   end
 
   def destroy
+    @user = current_user
+    if @user.destroy
+      redirect_to root_path, notice: "削除が完了しました"
+    else
+      redirect_to root_path, alert: "削除が失敗しました"
+    end
   end
 
   private
--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -2,20 +2,20 @@ require 'rails_helper'
 
 RSpec.describe "UserのE2Eテスト", type: :system do
   describe "GET /home" do
-    let(:user) {build(:user)}
+    let(:signup_user) {build(:user)}
 
     it "新規登録、ログアウト、ログインを実行すること" do
       visit new_user_registration_path
-      fill_in "Email",  with: user.email
-      fill_in "Password", with: user.password
-      fill_in "Password confirmation", with: user.password
+      fill_in "Email",  with: signup_user.email
+      fill_in "Password", with: signup_user.password
+      fill_in "Password confirmation", with: signup_user.password
       click_on 'Sign up'
       expect(current_path).to eq(root_path)
       click_on 'ログアウト'
       expect(current_path).to eq(root_path)
       click_on 'ログイン'
-      fill_in "Email",  with: user.email
-      fill_in "Password", with: user.password
+      fill_in "Email",  with: signup_user.email
+      fill_in "Password", with: signup_user.password
       click_on 'Log in'
       expect(current_path).to eq(root_path)
     end
