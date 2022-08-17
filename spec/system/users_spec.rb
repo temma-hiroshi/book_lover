b65a7d764002b3827045401f8b695d6708f0a39e
天摩洋 <chinmi@tenmayounoMacBook-Pro.local>
2022-08-17 20:25:06 +0900

    models,systemのuser,rspecを追加

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -3,21 +3,28 @@ require 'rails_helper'
 RSpec.describe "UserのE2Eテスト", type: :system do
   describe "GET /home" do
     let(:signup_user) {build(:user)}
+    let(:user) {create(:user)}
 
-    it "新規登録、ログアウト、ログインを実行すること" do
+    it "新規登録を実行すること" do
       visit new_user_registration_path
+      fill_in "Name",  with: signup_user.name
       fill_in "Email",  with: signup_user.email
       fill_in "Password", with: signup_user.password
       fill_in "Password confirmation", with: signup_user.password
       click_on 'Sign up'
-      expect(current_path).to eq(root_path)
-      click_on 'ログアウト'
-      expect(current_path).to eq(root_path)
-      click_on 'ログイン'
-      fill_in "Email",  with: signup_user.email
-      fill_in "Password", with: signup_user.password
+      expect(User.all.count).to eq 1
+    end
+
+    it "ログイン、ログアウトを実行すること" do
+      visit new_user_session_path
+      fill_in "Email",  with: user.email
+      fill_in "Password", with: user.password
       click_on 'Log in'
       expect(current_path).to eq(root_path)
+      expect(page).to have_content("Signed in successfully.")
+      click_on "ログアウト"
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Signed out successfully")
     end
   end
 end
