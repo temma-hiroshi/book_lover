3a4c08892df9e23eace22fa2c151c3d243fd74a0
天摩洋 <chinmi@tenmayounoMacBook-Pro.local>
2022-08-17 09:20:04 +0900

    deviseのマイグレーション修正及びUserモデルの作成

--- db/migrate/20220817001126_devise_create_users.rb
+++ db/migrate/20220817001126_devise_create_users.rb
@@ -34,6 +34,10 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.1]
 
 
       t.timestamps null: false
+
+      t.string :name
+      t.integer :book_id
+      t.integer :comment_id
     end
 
     add_index :users, :email,                unique: true
