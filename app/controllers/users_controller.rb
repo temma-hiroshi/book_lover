f5590572f0cc92a9a376282add9b3e0c26cddc58
天摩洋 <chinmi@tenmayounoMacBook-Pro.local>
2022-08-17 13:42:20 +0900

    users/editの処理を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,22 @@
 class UsersController < ApplicationController
   def edit
+    @user = current_user
   end
 
   def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to home_index_path
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
+
+  private
+    def user_params
+      params.require(:user).permit(:name, :image)
+    end
 end
