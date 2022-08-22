23ba03679786fdadcccef943314958220fd0e07e
天摩洋 <chinmi@tenmayounoMacBook-Pro.local>
2022-08-22 14:27:02 +0900

    bookモデルのrspecテストを追加

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :book do
-    title { "MyString" }
+    title { "title" }
+    author { "author" }
+    description { "description" }
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "Bookモデルのテスト" do
+    let(:book) {build(:book) }
+    let(:no_title_book) {build(:book, title: "") }
+    let(:no_author_book) {build(:book, author: "") }
+    let(:no_description_book) {build(:book, description: "") }
+
+    it "タイトル、著者、説明文がある場合は登録すること" do
+      expect(book).to be_valid
+    end
+
+    it "タイトルがない場合、登録しないこと" do
+      expect(no_title_book).to be_invalid
+    end
+
+    it "著者がない場合は登録しないこと" do
+      expect(no_author_book).to be_invalid
+    end
+
+    it "説明文がない場合は登録しないこと" do
+      expect(no_description_book).to be_invalid
+    end
+  end
 end
