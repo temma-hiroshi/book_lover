07f21a582df549563d23a0364c4dcde7130ca5fc
天摩洋 <chinmi@tenmayounoMacBook-Pro.local>
2022-08-26 15:14:49 +0900

    検索フォームを追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,16 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :set_search
+
+  def search
+    @results = @search.result
+  end
 
   private
+
+  def set_search
+    @search = Book.ransack(params[:q])
+  end
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,9 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   get 'home/index'
   resources :users, :only => [:show, :edit, :update, :destroy]
-  resources :books, :only => [:show, :new, :create, :edit, :update, :destroy]
+  resources :books, :only => [:show, :new, :create, :edit, :update, :destroy] do
+    collection do
+      get "search"
+    end
+  end
 end
